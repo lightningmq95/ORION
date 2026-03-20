2a124686a8d7bc959068b86cf504b0e51600dfd1
Akash Mohapatra <akashmahapatra762@gmail.com>
2026-03-20 22:45:45 +0530

    add: added mapper_node, works all good on ground_truth data. Have to implement extended kalman filter now.

--- simple_robot_control/setup.py
+++ simple_robot_control/setup.py
@@ -24,6 +24,7 @@ setup(
         'console_scripts': [
             'robot_subscriber = simple_robot_control.robot_subscriber:main',
             'ground_truth = simple_robot_control.ground_truth_node:main',
+            'mapper_node = simple_robot_control.mapper_node:main',
         ],
     },
 )
